e3fda0f1f8e680545dd79fe5a39f160bbbac53b3
CreateRandom <createrandom1@gmail.com>
2018-01-25 14:10:58 +0100

    Added conv networks :) ^2

--- Encoder.py
+++ Encoder.py
@@ -10,18 +10,34 @@ class Encoder(Chain):
         self.non_linearity = non_linearity
 
         with self.init_scope():
-            self.l0 = L.Linear(None, self.n_hidden)
-            self.l1 = L.Linear(self.n_hidden, self.n_hidden)
-            self.bn = L.BatchNormalization(self.n_hidden)
-            self.l2 = L.Linear(self.n_hidden, latent_dim)
+            # paper
+
+            # self.l0 = L.Linear(None, self.n_hidden)
+            # self.l1 = L.Linear(self.n_hidden, self.n_hidden)
+            # self.bn = L.BatchNormalization(self.n_hidden)
+            # self.l2 = L.Linear(self.n_hidden, latent_dim)
+
+            # conv. experiment
+            self.l0 = L.Convolution2D(None, 1, ksize=5, stride=2)
+            self.l1 = L.Linear(None, self.n_hidden)
+            self.l2 = L.Linear(None, self.n_hidden)
+            self.l3 = L.Linear(None, latent_dim)
 
     def __call__(self, x):
         # -1 --> filler for arbitrary number of elements, 1 * 28 * 28 --> one color channel, 28 by 28 pixels
-        x = F.reshape(x, (-1, 28*28))
-        l0_out = self.non_linearity(self.l0(x))
-        l1_out = self.l1(l0_out)
-        bn_out = self.non_linearity(self.bn(l1_out))
-        l2_out = self.l2(bn_out)
-        y = F.leaky_relu(l2_out, 0.2)
+        #x = F.reshape(x, (-1, 28*28))
+        # paper
+
+        # l0_out = self.non_linearity(self.l0(x))
+        # l1_out = self.l1(l0_out)
+        # bn_out = self.non_linearity(self.bn(l1_out))
+        # l2_out = self.l2(bn_out)
+        # y = F.leaky_relu(l2_out, 0.2)
+
+        # conv. experiment
+
+        re = F.relu(self.l0(F.reshape(x, (-1, 1, 28, 28))))
+        h1 = F.relu(self.l2(re))
+        y = self.l3(h1)
 
         return y
--- Generator.py
+++ Generator.py
@@ -8,18 +8,37 @@ class Generator(Chain):
 
         self.n_hidden = n_hidden
         self.non_linearity = non_linearity
+        self.in_channels  = 30
+        self.img_size = 28
         with self.init_scope():
+            # paper
+            # self.l0 = L.Linear(None, self.n_hidden)
+            # self.l1 = L.Linear(self.n_hidden, self.n_hidden)
+            # self.bn = L.BatchNormalization(self.n_hidden)
+            # self.l2 = L.Linear(self.n_hidden, 28*28)
 
-            self.l0 = L.Linear(None, self.n_hidden)
-            self.l1 = L.Linear(self.n_hidden, self.n_hidden)
-            self.bn = L.BatchNormalization(self.n_hidden)
-            self.l2 = L.Linear(self.n_hidden, 28*28)
+            # conv. exper
+            self.l0 = L.Linear(None, self.in_channels*self.img_size*self.img_size)
+            self.l1 = L.BatchNormalization(self.in_channels*self.img_size*self.img_size)
+            self.l2 = L.Deconvolution2D(self.in_channels, 1, ksize=3, outsize=(self.img_size,self.img_size), stride=1, pad=1)
 
     def __call__(self, x):
-        l0_out = self.non_linearity(self.l0(x))
-        l1_out = self.l1(l0_out)
-        bn_out = self.non_linearity(self.bn(l1_out))
-        y = F.relu(self.l2(bn_out))
+        # paper
+        # l0_out = self.non_linearity(self.l0(x))
+        # l1_out = self.l1(l0_out)
+        # bn_out = self.non_linearity(self.bn(l1_out))
+        # y = F.relu(self.l2(bn_out))
+
+        # conv. exper
+        # fully connected relu layer
+        h0 = self.l0(x)
+        h1 = self.non_linearity(self.l1(h0))
+        # reshape for deconv layer
+        h1 = F.reshape(h1,(-1, self.in_channels, self.img_size,self.img_size))
+        # sigmoid deconv
+        h2 = self.l2(h1)
+        y = F.sigmoid(h2)
+
         y = F.reshape(y, (-1, 1, 28, 28))
 
         return y
